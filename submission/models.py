89332f93fb8f2d2dcd0d3f53e2682f41ddedc942
Reagan Zierke <reaganzierke@gmail.com>
2025-08-12 17:44:18 -0500

    minor changes

--- submission/models.py
+++ submission/models.py
@@ -18,7 +18,7 @@ class Submission(models.Model):
 
     title = models.CharField(max_length=200, verbose_name='Title', help_text='Enter the title of the announcement')
     email = models.EmailField(max_length=254, verbose_name='Contact Email', help_text='Enter the email address to contact for any questions', blank=True, null=True)
-    description = models.TextField(verbose_name='Description', help_text='Enter the description of the announcement')
+    description = models.TextField(verbose_name='Description', help_text='Enter the description of the announcement', blank=True, null=True)
     start_date = models.DateTimeField(verbose_name='Start Date', help_text='Enter the start date of the announcement')
     end_date = models.DateTimeField(verbose_name='End Date', help_text='Enter the end date of the announcement')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At', help_text='The date and time when the announcement was created')
