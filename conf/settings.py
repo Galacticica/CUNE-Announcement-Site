623898a26bacdcdba3701bdac14adc7bedde1893
Reagan Zierke <reaganzierke@gmail.com>
2025-08-20 12:58:43 -0500

    added env vars

--- conf/settings.py
+++ conf/settings.py
@@ -9,6 +9,7 @@ Description: Django settings for the project, including database configuration, 
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0trsu0_)k^0bk9^h^ta_&$nin*%&azd0&_gl8yavyoq+m&bq9%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
+DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = []
 
